5b73b8a130755d8ae85330ef93adbd8b171eb24d
zhu733756 <talonzhu@yunify.com>
2021-06-28 14:36:02 +0800

    comment useless code
    Signed-off-by: zhu733756 <talonzhu@yunify.com>

--- api/v1alpha1/panels/common_types.go
+++ api/v1alpha1/panels/common_types.go
@@ -2,12 +2,12 @@
 
 package panels
 
-// Query editor options
-type CommonPanel struct {
-	// Name of the  panel
-	Title string `json:"title,omitempty" yaml:"title,omitempty"`
-	// Type of the  panel
-	Type string `json:"type,omitempty" yaml:"type,omitempty"`
-	// Panel ID
-	Id int64 `json:"id,omitempty" yaml:"id,omitempty"`
-}
+// // Query editor options
+// type CommonPanel struct {
+// 	// Name of the  panel
+// 	Title string `json:"title,omitempty" yaml:"title,omitempty"`
+// 	// Type of the  panel
+// 	Type string `json:"type,omitempty" yaml:"type,omitempty"`
+// 	// Panel ID
+// 	Id int64 `json:"id,omitempty" yaml:"id,omitempty"`
+// }
--- api/v1alpha1/panels/singlestat_types.go
+++ api/v1alpha1/panels/singlestat_types.go
@@ -2,35 +2,35 @@
 
 package panels
 
-// SingleStat shows instant query result
-type SingleStat struct {
-	// Name of the signlestat panel
-	Title string `json:"title,omitempty" yaml:"title,omitempty"`
-	// Must be `singlestat`
-	Type string `json:"type,omitempty" yaml:"type,omitempty"`
-	// Panel ID
-	Id int64 `json:"id,omitempty" yaml:"id,omitempty"`
-	// A collection of queries
-	Targets []Target `json:"targets,omitempty" yaml:"targets,omitempty"`
-	// Limit the decimal numbers
-	Decimals int64 `json:"decimals,omitempty" yaml:"decimals,omitempty"`
-	// Display unit
-	Format string `json:"format,omitempty" yaml:"format,omitempty"`
-	// datasource
-	Datasource string `json:"datasource,omitempty" yaml:"datasource,omitempty"`
-	// colors
-	Colors []string `json:"colors,omitempty" yaml:"colors,omitempty"`
-	// color settings
-	Color []string `json:"color,omitempty" yaml:"color,omitempty"`
-	// spark line: full or bottom
-	SparkLine string `json:"sparkline" yaml:"sparkline"`
-	// height
-	Height string `json:"height,omitempty" yaml:"height,omitempty"`
-	// gauge
-	Gauge Gauge `json:"gauge,omitempty" yaml:"gauge,omitempty"`
-	// value name
-	ValueName string `json:"valueName,omitempty" yaml:"valueName,omitempty"`
-}
+// // SingleStat shows instant query result
+// type SingleStat struct {
+// 	// Name of the signlestat panel
+// 	Title string `json:"title,omitempty" yaml:"title,omitempty"`
+// 	// Must be `singlestat`
+// 	Type string `json:"type,omitempty" yaml:"type,omitempty"`
+// 	// Panel ID
+// 	Id int64 `json:"id,omitempty" yaml:"id,omitempty"`
+// 	// A collection of queries
+// 	Targets []Target `json:"targets,omitempty" yaml:"targets,omitempty"`
+// 	// Limit the decimal numbers
+// 	Decimals int64 `json:"decimals,omitempty" yaml:"decimals,omitempty"`
+// 	// Display unit
+// 	Format string `json:"format,omitempty" yaml:"format,omitempty"`
+// 	// datasource
+// 	Datasource string `json:"datasource,omitempty" yaml:"datasource,omitempty"`
+// 	// colors
+// 	Colors []string `json:"colors,omitempty" yaml:"colors,omitempty"`
+// 	// color settings
+// 	Color []string `json:"color,omitempty" yaml:"color,omitempty"`
+// 	// spark line: full or bottom
+// 	SparkLine string `json:"sparkline" yaml:"sparkline"`
+// 	// height
+// 	Height string `json:"height,omitempty" yaml:"height,omitempty"`
+// 	// gauge
+// 	Gauge Gauge `json:"gauge,omitempty" yaml:"gauge,omitempty"`
+// 	// value name
+// 	ValueName string `json:"valueName,omitempty" yaml:"valueName,omitempty"`
+// }
 
 // Gauge for a stat
 type Gauge struct {
